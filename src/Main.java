c82c8dbbc4642cc350679ae0931ec48fbe6d0418
Artem Kozyr <zymber6@yahoo.com>
2020-06-19 01:36:26 +0300

    Tried some combination generators.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         Setup setup = new Setup();
-        System.out.println(setup.possibleFilmsToWatch(340));
+
+        System.out.println(setup.possibleFilmsToWatch(280));
+
+        List<String> result = new ArrayList<>();
+
+        //System.out.println(setup.generatePermutations(setup.possibleFilmsToWatch(340), result, 0, ""));
+
+        //System.out.println(setup.getAllCombinations(setup.possibleFilmsToWatch(340)));
     }
 }
--- src/Setup.java
+++ src/Setup.java
@@ -6,7 +6,7 @@ import java.util.*;
 public class Setup {
     private static final String fileURL = "D:\\Study\\projectDayTask\\src\\MovieList.txt";
 
-    public int possibleFilmsToWatch(int timeInTrain){
+    public String possibleFilmsToWatch(int timeInTrain) {
 
         ArrayList<Film> filmList = new ArrayList<>();
 
@@ -35,7 +35,7 @@ public class Setup {
         //filmList.sort(new ComparatorSetup());
         Collections.sort(filmList, new ComparatorSetup());
 
-        System.out.println("Sorted list of films: ");
+        System.out.println("\n Sorted list of films: ");
         for (Film st : filmList)
             System.out.println(st);
 
@@ -54,10 +54,79 @@ public class Setup {
             }
         }
 
-        System.out.println("You can watch: ");
-        for (Film st : possibleFilmsToWatch)
-            System.out.println(st);
+        /*int counter = 0;
+        int MIN_VAL = 3;
 
-        return filmCounter;
+        for (int i = 1; i < )
+
+
+        while (filmList.get(counter).getFilm_size() < timeInTrain || counter != filmList.size() && counter > MIN_VAL) {
+            Set<String> allFilms = new HashSet<>();
+            timeInTrain -= filmList.get(counter).getFilm_size();
+            allFilms.add(filmList.get(counter).getFilm_name());
+            possibleFilmsToWatch.addAll(allFilms);
+            counter++;
+        }*/
+
+        /*System.out.println("\n You can watch: ");
+        for (String st : possibleFilmsToWatch)
+            System.out.println(st);*/
+
+        /*String sArray[] = new String []{"A", "A", "B", "C"};
+        //convert array to list
+        List<String> list1 = Arrays.asList(sArray);
+        List<String> list2 = Arrays.asList(sArray);
+        List<String> list3 = Arrays.asList(sArray);
+
+        LinkedList<List <String>> lists = new LinkedList<List <String>>();
+
+        lists.add(list1);
+        lists.add(list2);
+        lists.add(list3);
+
+        Set<String> combinations = new TreeSet<String>();
+        Set<String> newCombinations;
+
+        for (String s: lists.removeFirst())
+            combinations.add(s);
+
+        while (!lists.isEmpty()) {
+            List<String> next = lists.removeFirst();
+            newCombinations =  new TreeSet<String>();
+            for (String s1: combinations)
+                for (String s2 : next)
+                    newCombinations.add(s1 + s2);
+
+            combinations = newCombinations;
+        }
+        for (String s: combinations)
+            System.out.print(s+" ");*/
+
+        return possibleFilmsToWatch.toString();
     }
+
+    /*public List<List<String>> getAllCombinations(List<String> elements) {
+        List<List<String>> combinationList = new ArrayList<List<String>>();
+        for (long i = 1; i < Math.pow(2, elements.size()); i++) {
+            List<String> list = new ArrayList<String>();
+            for (int j = 0; j < elements.size(); j++) {
+                if ((i & (long) Math.pow(2, j)) > 0) {
+                    list.add(elements.get(j));
+                }
+            }
+            combinationList.add(list);
+        }
+        return combinationList;
+    }*/
+
+   /*public void generatePermutations(List<List<Character>> lists, List<String> result, int depth, String current) {
+        if (depth == lists.size()) {
+            result.add(current);
+            return;
+        }
+
+        for (int i = 0; i < lists.get(depth).size(); i++) {
+            generatePermutations(lists, result, depth + 1, current + lists.get(depth).get(i));
+        }
+    }*/
 }
